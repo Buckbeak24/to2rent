5f617082e18916364052a2f777e33beae3793b06
Buckbeak95 <gorillaz.1793@gmail.com>
2021-01-15 23:02:09 +0300

    website adaptive created

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-$(function(){
+$(function () {
     $('.slider').slick({
         nextArrow: '<button class="slick-arrow slick-next"><img src="/images/next.svg" alt="next arrow"></button>',
         prevArrow: '<button class="slick-arrow slick-prev"><img src="/images/prev.svg" alt="prev arrow"></button>',
-        fade: true
+        fade: true,
+        responsive: [
+            {
+                breakpoint: 441,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     });
 });
